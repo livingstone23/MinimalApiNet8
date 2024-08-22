f45135c108eca11d10c6be3656430cd8438d7b8a
Livingstone Cano <livingstone23@gmail.com>
2024-08-22 15:08:08 +0200

    Enable CORS and Cache

--- MangoFinancialApi/Program.cs
+++ MangoFinancialApi/Program.cs
@@ -1,44 +1,88 @@
+using MangoDomain.EntititesTest;
+using Microsoft.AspNetCore.Cors;
+
+
+
 var builder = WebApplication.CreateBuilder(args);
+var origenPermited = builder.Configuration.GetValue<string>("origenPermited")!;
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+    //Habilitamos CORS
+    builder.Services.AddCors(options =>
+    {
+        options.AddDefaultPolicy(configuration =>
+        {
+            configuration.WithOrigins(origenPermited)
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+        //Add another politics
+        options.AddPolicy("free",configuration =>
+        {
+            configuration
+                .AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+    });
 
-app.UseHttpsRedirection();
+    //Enable the use of CACHE, here activate the cache for all the endpoints
+    builder.Services.AddOutputCache();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+    //End of services area
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
 
+    var app = builder.Build();
+    //Start area of middleware configuration
+
+
+    //Enable CORS
+    app.UseCors();
+
+
+    //Enable the use of CACHE
+    app.UseOutputCache();
+    
+
+    //Habilito en endpoint libre acceso
+    app.MapGet("/", [EnableCors(policyName: "free")]() => "!Hello World!");
+
+
+    app.MapGet("/genders",() => 
+    {
+        var genders = new List<Gender>
+        {
+            new Gender {Id = 1, Name = "Action"},
+            new Gender {Id = 2, Name = "Comedy"},
+            new Gender {Id = 3, Name = "Horror"}
+        };
+
+        return genders;
+    }).CacheOutput(c => c.Expire(TimeSpan.FromHours(1)));
+        
+
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+
+    
+
+
+
+
+//End area of middleware configuration
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+
